6157b1f802f875cfb4543ca23cb2a2a5e026ca17
BuzJr <112666026+BuzJr@users.noreply.github.com>
2023-08-22 10:05:09 -0400

    Implemented Ray Intersection Formula

--- RayTracer/src/Renderer.cpp
+++ RayTracer/src/Renderer.cpp
@@ -1,15 +1,6 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
-void Renderer::Render()
-{
-	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++) {
-		m_ImageData[i] = Walnut::Random::UInt();
-		m_ImageData[i] |= 0xff000000;
-	}
-
-	m_FinalImage->SetData(m_ImageData);
-}
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
@@ -30,3 +21,55 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
 }
+
+void Renderer::Render()
+{
+	//Iterate through and render every pixel
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) {
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
+			glm::vec2 coord = { x / (float)m_FinalImage->GetWidth(), y / (float)m_FinalImage->GetHeight() };
+			coord = coord * 2.0f - 1.0f; //Maps coordinate -1 to 1
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+		}
+	}
+
+	m_FinalImage->SetData(m_ImageData);
+}
+
+uint32_t Renderer::PerPixel(glm::vec2 coord)
+{
+	uint8_t r = (uint8_t)(coord.x * 255.0f);
+	uint8_t g = (uint8_t)(coord.y * 255.0f);
+
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDir(coord.x, coord.y, -1.0f);
+	float radius = 0.5f;
+
+	//Formula for finding the distance to an intersection of a sphere (centered on origin)
+	//(bx^2+by^2+bz^2)t^2 + (2axbx + 2ayby + 2azbz)t + (ax^2 + ay^2 + az^2 - r^2) = 0
+	//a = ray origin
+	//b = ray direction
+	//r = circle radius
+	//t = dist to hit (from a)
+
+	//Coefecients of our quadratic equation: ax^2 + bx + c = 0
+	float a = glm::dot(rayDir, rayDir);
+	float b = 2.0f * glm::dot(rayOrigin, rayDir);
+	float c = glm::dot(rayOrigin, rayOrigin) - (radius * radius);
+
+	//Quadratic Discriminant:
+	// b^2 - 4ac
+	float discriminant = (b * b) - 4.0f * (a * c);
+
+	//Check for intersections
+	//discrim > 0: 2 solutions
+	//discrim = 0: 1 solution
+	//discrim < 0: 0 solutions
+	if (discriminant >= 0.0f)
+	{
+		return 0xffff00ff;
+	}
+
+	return 0xffffffff;
+}
+
