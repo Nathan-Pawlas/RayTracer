d81a5c41eb30a457d4a095cf3d58bf870ad5b47b
BuzJr <112666026+BuzJr@users.noreply.github.com>
2023-08-24 20:48:47 -0400

    Added Light Direction and Shading based on dotproduct of surface normal and light

--- RayTracer/src/Renderer.cpp
+++ RayTracer/src/Renderer.cpp
@@ -2,6 +2,16 @@
 #include "Walnut/Random.h"
 
 
+namespace Utils {
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		//Written on oneline for optimization: 
+		// for each rgba channel it's multiplied by f (255) then converted to byte, giving a value between 0 & f
+		// it is then bit shifted into the corresponding 32 bit channel (0x aa bb gg rr)
+		return ((uint8_t)(color.a * 255.0f) << 24) | ((uint8_t)(color.b * 255.0f) << 16) | ((uint8_t)(color.g * 255.0f) << 8) | (uint8_t)(color.r * 255.0f);
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -31,19 +41,21 @@ void Renderer::Render()
 			glm::vec2 coord = { x / (float)m_FinalImage->GetWidth(), y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f; //Maps coordinate -1 to 1
 			coord.x *= aspeectRatio;
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
 	m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
 
-	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	glm::vec3 rayDir(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 
@@ -66,16 +78,22 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	//discrim > 0: 2 solutions
 	//discrim = 0: 1 solution
 	//discrim < 0: 0 solutions
-	if (discriminant >= 0.0f)
-	{
-		//completing the Quadratic Formula:
-		//(-b +- sqrt(dircriminant)) / (2.0f * a)
-		float t0 = (-b - glm::sqrt(discriminant)) / (2.0f * a);
-		float t1 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+	if (discriminant < 0.0f)
+		return glm::vec4(0,0,0,1); //No solutions, early return black
 
-		return 0xffff00ff;
-	}
+	//completing the Quadratic Formula:
+	//(-b +- sqrt(dircriminant)) / (2.0f * a)
+	float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+	float t1 = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+	//t1 < t0, a is squared so it can't be neg., thus because b is subtracted in t1, t1 must always be smaller than t0
+	glm::vec3 hitPoint = rayOrigin + rayDir * t1;
+	glm::vec3 normal = glm::normalize(hitPoint);
+	glm::vec3 lightDir = glm::normalize(glm::vec3(1, 1, 1));
 
-	return 0xffffffff;
+	float dotProduct = glm::max(glm::dot(normal, lightDir),0.0f); // = cos(angle)
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= dotProduct;
+	return glm::vec4(sphereColor,1);
 }
 
